b70a01a725d91c03761b363dd4d393423f8cf54e
manzanillo <manza@outlook.de>
2019-09-19 10:40:23 +0200

    added websocket

---
+++ backend/index.js
@@ -0,0 +1,52 @@
+const WebSocket = require('ws')
+
+const http = require('http')
+//TODO: Security
+//const fs = require('fs');
+//const https = require('https');
+
+/*const server = https.createServer({
+  cert: fs.readFileSync('/path/to/cert.pem'),
+  key: fs.readFileSync('/path/to/key.pem')
+})*/
+
+const actions = [
+  'smiling',
+  'checkright',
+  'checkleft',
+  'giggling',
+  'dubitative',
+  'sad',
+  'reallysad',
+  'bored',
+  'dubitative',
+  'chock',
+  'suspicious',
+  'satisfied',
+  'pong',
+  'killthehumans',
+  'hal9000'
+]
+
+let CLIENTS = []
+
+const server = http.createServer()
+
+const wss = new WebSocket.Server({ port: 8080 })
+
+wss.on('connection', function connection(ws, request, client) {
+  CLIENTS[request.url] = ws
+  CLIENTS.push(ws)
+
+  console.log(request.url)
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message)
+    const action = 'jump'
+    //ws.send(action)
+    for (var j = 0; j < CLIENTS.length; j++) {
+      CLIENTS[j].send(message)
+    }
+  })
+})
+
+//server.listen(8080)
