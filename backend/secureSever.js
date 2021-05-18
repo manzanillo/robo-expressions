217b4b1c834d90f340c9b8df4950556e1cf62e46
chikobam89 <chikobam89@mi.fu-berlin.de>
2021-05-18 19:28:55 +0200

    parse room

--- backend/secureSever.js
+++ backend/secureSever.js
@@ -8,12 +8,50 @@ const server = https.createServer({
     passphrase: "12345"
 });
 
+const actions = [
+    "smiling",
+    "checkright",
+    "checkleft",
+    "giggling",
+    "dubitative",
+    "sad",
+    "reallysad",
+    "bored",
+    "dubitative",
+    "chock",
+    "suspicious",
+    "satisfied",
+    "pong",
+    "killthehumans",
+    "hal9000"
+]
+
+let CLIENTS = {}
+
 const wss = new WebSocket.Server({ server, port: 8080 });
 
-wss.on('connection', function connection(ws) {
-    ws.on('message', function incoming(message) {
-        console.log('received: %s', message);
-    });
+wss.on('connection', function connection(ws, request) {
+    // remove query params
+    let url = request.url.split("?")[0]
 
-    ws.send('something');
+    // users need to specify a custom room
+    if (url.length < 2) {
+        ws.close()
+    }
+
+    if (CLIENTS[url] && CLIENTS[url].length > 0) {
+        CLIENTS[url].push(ws)
+    }
+    else {
+        CLIENTS[url] = [ws]
+    }
+    console.log(url)
+    ws.on("message", function incoming(message) {
+        console.log("received: %s", message)
+        if (actions.indexOf(message.toLowerCase()) > -1) {
+            for (var j = 0; j < CLIENTS[url].length; j++) {
+                CLIENTS[url][j].send(message.toLowerCase())
+            }
+        }
+    })
 });
